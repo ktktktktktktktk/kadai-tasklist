16018d18e43510407d2b313637fd042c57f603a5
kt <202011251107a@gmail.com>
2020-12-15 10:07:12 +0000

    add colum

--- app/Http/Controllers/Taskscontroller.php
+++ app/Http/Controllers/Taskscontroller.php
@@ -43,9 +43,11 @@ class Taskscontroller extends Controller
     public function store(Request $request)
     {
         $request->validate([
+            "title"   => "required|max:255",
             "content" => "required|max:255",
             ]);
         $task = new Task;
+        $task -> title   = $request -> title;
         $task -> content = $request -> content;
         $task -> save();
         
@@ -90,10 +92,11 @@ class Taskscontroller extends Controller
     public function update(Request $request, $id)
     {
         $request->validate([
+            "title"   => "required|max:255",
             "content" => "required|max:255",
             ]);
         $task = Task::findOrFail($id);
-        
+        $task->title   = $request->title;
         $task->content = $request->content;
         $task->save();
         
